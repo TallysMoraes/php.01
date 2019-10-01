92e3f6efa146dfca68753d488b5fab36424456e4
tallys augusto <tallysmugetsu@gmail.com>
2019-09-30 21:41:38 -0300

    crindo formulario php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpah.bootstrapcdn.com/bootstrep/4.3.1/css/booststrap.min.css" integrity="sha384-ggOyR0ixCbMQv3Xipma34MD+DH/1fQ784/j6cY/iJTQUOhcWr7x9TvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="styler.css">
+    <title>Sistema Web</title>
+</head>
+<body>
+    <Div class="container">
+        <h2>Cadastro de Cliente</h2>
+        <form action="" style="width:500px;">
+            <label for="">Nome</label>
+            <input type="text" class="form-control">
+            <label for="">Idade</label>
+            <input type="number" class="form-control">
+            <label for="">E-mail</label>
+            <input type="email" class="form-control">
+            <br>
+            <input type="submit" class="btn btn-primary">
+        </form>
+    </Div>
+</body>
+</html>
